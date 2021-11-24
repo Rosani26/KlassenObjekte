e69f0ec4b44f291de00b19e4869612ec628e6af7
Rosani26 <94964550+Rosani26@users.noreply.github.com>
2021-11-24 13:30:52 +0100

    Klassen und Objekte
    Klassen und Objekte

---
+++ src/main/java/Application.java
@@ -0,0 +1,26 @@
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
+
+
+// Plan / Class of the house
+public class House {
+
+    //Attributes of the house
+    private String familyName;
+    private String steet;
+    private int houseNumber;
+    private int numberOfRooms;
+    private double price;
+
+
+    // initializes a new house-object
+    public House() {
+        familyName = ""; // we dont know yet, we set to an initial value
+        street =
+    }
+}
---
+++ src/main/java/House.java
@@ -0,0 +1,59 @@
+public class House {
+
+package com.example.class_objects;
+
+// Plan / Class of the house
+    public class {
+    private final;
+    // Attributes of the house
+    private String familyName;
+    private String street;
+    private int houseNumber;
+    private double price;
+
+ // initializer a new house-object
+ public House() {
+     familyName = ""; //we dont know yet, we set to an initial value
+     street = "";
+     houseNumber = 0;
+     numberOfRooms = 0;
+     price=0.0;
+ }
+
+
+ // Plan / Class of the house
+ public class House {
+     // Attributes of the house
+     private String familyName;
+     private String street;
+     private int houseNumber;
+     private int numberOfRooms;
+     private double price;
+
+
+     // initializes a new house-object
+     //the constructor
+     public House() {
+         familyName= ""; // we dont know yet, we set to an initial value
+         street = "";
+         houseNumber = 0;
+         numberOfRooms = 0;
+         price=0.0;
+     }
+     // getter and setter here
+     public String getFamilyName() {
+         return familyName;
+     }
+
+     public void setFamilyName(String familyName) {
+         this.familyName = familyName;
+     }
+
+ }
+
+
+}
+
+
+
+}
